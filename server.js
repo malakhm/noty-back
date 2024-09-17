14f604494f15a7dc1e3986030c0e345e94a79dd8
malakhm <malakhmm22@gmail.com>
2024-09-17 11:00:14 +0300

    fixed cors error

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ import NotesRouter from './Routers/Notes.js';
 dotenv.config()
 const app = express();
 app.use(express.json());
-app.use(cors())
+app.use(cors("*"))
 app.use('/api/note', NotesRouter);
 
 mongoose.connect(process.env.MONGO_URI)
